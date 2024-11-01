44ef5c409b09a28b8dc4aba28c85d80130b64b96
Richard Iannone <riannone@me.com>
2024-10-31 22:32:29 -0400

    Update __init__.py

--- pointblank/__init__.py
+++ pointblank/__init__.py
@@ -6,6 +6,6 @@ except PackageNotFoundError:
     __version__ = "0.0.0"  # Default version if metadata is not found
 
 # Import objects from the module
-from .test_col_vals_gt import test_col_vals_gt
+from .test import Test
 
-__all__ = ["test_col_vals_gt"]
+__all__ = ["Test"]
