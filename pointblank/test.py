f16bedd7211840338b6d13301f33282905fea845
Richard Iannone <riannone@me.com>
2024-10-31 22:34:19 -0400

    Add `_col_vals_compare_one()` util fn

--- pointblank/test.py
+++ pointblank/test.py
@@ -127,3 +127,74 @@ def _col_vals_compare_set_docstring(inside: bool) -> str:
 
     comparison = "in" if inside else "not in"
     return COL_VALS_COMPARE_SET_DOCSTRING.replace("___", comparison)
+
+
+def _col_vals_compare_one(
+    df: FrameT,
+    column: str,
+    value: float | int,
+    threshold: int,
+    comparison: str,
+    type: str = "numeric",
+) -> bool:
+    """
+    General routine to compare values in a column against a single value.
+
+    Parameters
+    ----------
+    df : FrameT
+        a DataFrame.
+    column : str
+        The column to check.
+    value : float | int
+        A value to check against.
+    threshold : int
+        The maximum number of failing test units to allow.
+    comparison : str
+        The type of comparison ('gt' for greater than, 'lt' for less than).
+    type : str
+        The data type of the column.
+
+    Returns
+    -------
+    bool
+        `True` when test units pass below the threshold level for failing test units, `False`
+        otherwise.
+    """
+
+    # Convert the DataFrame to a format that narwhals can work with and:
+    #  - check if the column exists
+    #  - check if the column type is compatible with the test
+    dfn = column_test_prep(df=df, column=column, type=type)
+
+    # Collect results for the test units; the results are a list of booleans where
+    # `True` indicates a passing test unit
+    if comparison == "gt":
+        test_unit_res = Comparator(x=dfn, column=column, compare=value).gt()
+    elif comparison == "lt":
+        test_unit_res = Comparator(x=dfn, column=column, compare=value).lt()
+    elif comparison == "eq":
+        test_unit_res = Comparator(x=dfn, column=column, compare=value).eq()
+    elif comparison == "ne":
+        test_unit_res = Comparator(x=dfn, column=column, compare=value).ne()
+    elif comparison == "ge":
+        test_unit_res = Comparator(x=dfn, column=column, compare=value).ge()
+    elif comparison == "le":
+        test_unit_res = Comparator(x=dfn, column=column, compare=value).le()
+
+    else:
+        raise ValueError(
+            """Invalid comparison type. Use:
+            - `gt` for greater than,
+            - `lt` for less than,
+            - `eq` for equal to,
+            - `ne` for not equal to,
+            - `ge` for greater than or equal to, or
+            - `le` for less than or equal to."""
+        )
+
+    # Get the number of failing test units by counting instances of `False` and
+    # then determine if the test passes overall by comparing the number of failing
+    # test units to the threshold for failing test units
+    return threshold_check(failing_test_units=test_unit_res.count(False), threshold=threshold)
+
