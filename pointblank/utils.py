0d82f985396ae6da16fbcf5f057aa6ac5b9bebb6
Richard Iannone <riannone@me.com>
2024-10-31 22:30:38 -0400

    Add the `threshold_check()` util fn

--- pointblank/utils.py
+++ pointblank/utils.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import re
+
 import narwhals as nw
 from narwhals.typing import FrameT
 
@@ -25,4 +27,21 @@ def validate_numeric_column(dfn: FrameT, column: str) -> None:
 
     if not dfn.collect_schema().get(column).is_numeric:
         raise TypeError(f"Column '{column}' is not numeric.")
-    
+    def threshold_check(failing_test_units: int, threshold: int) -> bool:
+    """
+    Determine if the number of failing test units is below the threshold.
+
+    Parameters
+    ----------
+    failing_test_units
+        The number of failing test units.
+    threshold
+        The maximum number of failing test units to allow.
+
+    Returns
+    -------
+        `True` when test units pass below the threshold level for failing test units, `False`
+        otherwise.
+    """
+
+    return failing_test_units < threshold
