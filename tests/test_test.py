9bf82eaac6bbb3448d62560b225498dd2a9ece05
Richard Iannone <riannone@me.com>
2024-10-31 22:32:57 -0400

    Update test_test.py

--- tests/test_test.py
+++ tests/test_test.py
@@ -8,3 +8,17 @@ from pointblank.test import Test
 def tbl():
     return pd.DataFrame({"x": [1, 2, 3, 4], "y": [4, 5, 6, 7], "z": [8, 8, 8, 8]})
 
+
+def test_col_vals_gt(tbl):
+
+    assert Test.col_vals_gt(tbl, column="x", value=0, threshold=1) == True
+    assert Test.col_vals_gt(tbl, column="x", value=1, threshold=1) == False
+    assert Test.col_vals_gt(tbl, column="x", value=1, threshold=2) == True
+
+    assert Test.col_vals_gt(tbl, column="y", value=1, threshold=1) == True
+    assert Test.col_vals_gt(tbl, column="y", value=4, threshold=1) == False
+    assert Test.col_vals_gt(tbl, column="y", value=4, threshold=2) == True
+
+    assert Test.col_vals_gt(tbl, column="z", value=8, threshold=1) == False
+    assert Test.col_vals_gt(tbl, column="z", value=8, threshold=5) == True
+
