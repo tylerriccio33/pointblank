5e0d7e841476e038958b64635f03295ded46f4da
Richard Iannone <riannone@me.com>
2024-11-09 23:11:02 -0500

    Update __init__.py

--- pointblank/__init__.py
+++ pointblank/__init__.py
@@ -1,13 +1,13 @@
 from importlib_metadata import version, PackageNotFoundError
 
-try:
+try:  # pragma: no cover
     __version__ = version("pointblank")
-except PackageNotFoundError:
-    __version__ = "0.0.0"  # Default version if metadata is not found
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "0.0.0"
 
 # Import objects from the module
-from pointblank.test import Test
+from pointblank.tf import TF
 from pointblank.validate import Validate
 from pointblank.thresholds import Thresholds
 
-__all__ = ["Test", "Validate", "Thresholds"]
+__all__ = ["TF", "Validate", "Thresholds"]
