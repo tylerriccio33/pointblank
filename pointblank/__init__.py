12c2bd147351f2c54b17e729ca8fcca1dd451090
Richard Iannone <riannone@me.com>
2024-11-28 11:28:28 -0500

    Update try-except import

--- pointblank/__init__.py
+++ pointblank/__init__.py
@@ -1,4 +1,7 @@
-from importlib_metadata import version, PackageNotFoundError
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
 
 try:  # pragma: no cover
     __version__ = version("pointblank")
