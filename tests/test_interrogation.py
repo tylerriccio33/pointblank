6b542cc236b3a4409170472927a485cedd154de2
Richard Iannone <riannone@me.com>
2024-12-10 18:45:44 -0500

    Add more tests for the interrogation classes

--- tests/test_interrogation.py
+++ tests/test_interrogation.py
@@ -1,4 +1,28 @@
-from pointblank._interrogation import _get_nw_closed_str
+import pytest
+import pandas as pd
+import polars as pl
+
+from pointblank._interrogation import (
+    _get_nw_closed_str,
+    ColValsCompareOne,
+    ColValsCompareTwo,
+    ColValsCompareSet,
+    ColValsRegex,
+    ColExistsHasType,
+)
+
+
+@pytest.fixture
+def tbl_pd():
+    return pd.DataFrame({"x": [1, 2, 3, 4], "y": ["4", "5", "6", "7"], "z": [8, 8, 8, 8]})
+
+
+@pytest.fixture
+def tbl_pl():
+    return pl.DataFrame({"x": [1, 2, 3, 4], "y": ["4", "5", "6", "7"], "z": [8, 8, 8, 8]})
+
+
+COLUMN_LIST = ["x", "y", "z", "pb_is_good_"]
 
 
 def test_get_nw_closed_str():
@@ -7,3 +31,202 @@ def test_get_nw_closed_str():
     assert _get_nw_closed_str(closed=(True, False)) == "left"
     assert _get_nw_closed_str(closed=(False, True)) == "right"
     assert _get_nw_closed_str(closed=(False, False)) == "none"
+
+
+@pytest.mark.parametrize(
+    "tbl_fixture, assertion_method",
+    [
+        ("tbl_pd", "gt"),
+        ("tbl_pd", "lt"),
+        ("tbl_pd", "eq"),
+        ("tbl_pd", "ne"),
+        ("tbl_pd", "ge"),
+        ("tbl_pd", "le"),
+        ("tbl_pl", "gt"),
+        ("tbl_pl", "lt"),
+        ("tbl_pl", "eq"),
+        ("tbl_pl", "ne"),
+        ("tbl_pl", "ge"),
+        ("tbl_pl", "le"),
+    ],
+)
+def test_col_vals_compare_one(request, tbl_fixture, assertion_method):
+
+    tbl = request.getfixturevalue(tbl_fixture)
+
+    col_vals_compare_one = ColValsCompareOne(
+        data_tbl=tbl,
+        column="x",
+        value=1,
+        na_pass=True,
+        threshold=10,
+        assertion_method=assertion_method,
+        allowed_types=["numeric"],
+    )
+
+    assert isinstance(
+        col_vals_compare_one.test_unit_res,
+        pd.DataFrame if tbl_fixture == "tbl_pd" else pl.DataFrame,
+    )
+
+    if tbl_fixture == "tbl_pd":
+        assert col_vals_compare_one.test_unit_res.columns.tolist() == COLUMN_LIST
+        assert col_vals_compare_one.get_test_results().columns.tolist() == COLUMN_LIST
+    else:
+        assert col_vals_compare_one.test_unit_res.columns == COLUMN_LIST
+        assert col_vals_compare_one.get_test_results().columns == COLUMN_LIST
+
+
+def test_col_vals_compare_one_invalid_assertion_method(tbl_pd):
+
+    with pytest.raises(ValueError):
+        ColValsCompareOne(
+            data_tbl=tbl_pd,
+            column="x",
+            value=1,
+            na_pass=True,
+            threshold=10,
+            assertion_method="invalid",
+            allowed_types=["numeric"],
+        )
+
+
+@pytest.mark.parametrize(
+    "tbl_fixture, assertion_method",
+    [
+        ("tbl_pd", "between"),
+        ("tbl_pd", "outside"),
+        ("tbl_pl", "between"),
+        ("tbl_pl", "outside"),
+    ],
+)
+def test_col_vals_compare_two(request, tbl_fixture, assertion_method):
+
+    tbl = request.getfixturevalue(tbl_fixture)
+
+    col_vals_compare_two = ColValsCompareTwo(
+        data_tbl=tbl,
+        column="x",
+        value1=1,
+        value2=2,
+        inclusive=(True, True),
+        na_pass=True,
+        threshold=10,
+        assertion_method=assertion_method,
+        allowed_types=["numeric"],
+    )
+
+    assert isinstance(
+        col_vals_compare_two.test_unit_res,
+        pd.DataFrame if tbl_fixture == "tbl_pd" else pl.DataFrame,
+    )
+
+    if tbl_fixture == "tbl_pd":
+        assert col_vals_compare_two.test_unit_res.columns.tolist() == COLUMN_LIST
+        assert col_vals_compare_two.get_test_results().columns.tolist() == COLUMN_LIST
+    else:
+        assert col_vals_compare_two.test_unit_res.columns == COLUMN_LIST
+        assert col_vals_compare_two.get_test_results().columns == COLUMN_LIST
+
+
+def test_col_vals_compare_two_invalid_assertion_method(tbl_pd):
+
+    with pytest.raises(ValueError):
+        ColValsCompareTwo(
+            data_tbl=tbl_pd,
+            column="x",
+            value1=1,
+            value2=2,
+            inclusive=(True, True),
+            na_pass=True,
+            threshold=10,
+            assertion_method="invalid",
+            allowed_types=["numeric"],
+        )
+
+
+@pytest.mark.parametrize(
+    "tbl_fixture, inside",
+    [
+        ("tbl_pd", True),
+        ("tbl_pd", False),
+        ("tbl_pl", True),
+        ("tbl_pl", False),
+    ],
+)
+def test_col_vals_compare_set(request, tbl_fixture, inside):
+
+    tbl = request.getfixturevalue(tbl_fixture)
+
+    col_vals_compare_set = ColValsCompareSet(
+        data_tbl=tbl,
+        column="x",
+        values=[1, 2, 3],
+        threshold=10,
+        inside=inside,
+        allowed_types=["numeric"],
+    )
+
+    assert isinstance(
+        col_vals_compare_set.test_unit_res,
+        pd.DataFrame if tbl_fixture == "tbl_pd" else pl.DataFrame,
+    )
+
+    if tbl_fixture == "tbl_pd":
+        assert col_vals_compare_set.test_unit_res.columns.tolist() == COLUMN_LIST
+        assert col_vals_compare_set.get_test_results().columns.tolist() == COLUMN_LIST
+    else:
+        assert col_vals_compare_set.test_unit_res.columns == COLUMN_LIST
+        assert col_vals_compare_set.get_test_results().columns == COLUMN_LIST
+
+
+@pytest.mark.parametrize(
+    "tbl_fixture",
+    ["tbl_pd", "tbl_pl"],
+)
+def test_col_vals_regex(request, tbl_fixture):
+
+    tbl = request.getfixturevalue(tbl_fixture)
+
+    col_vals_regex = ColValsRegex(
+        data_tbl=tbl,
+        column="y",
+        pattern=r"^[0-9]$",
+        na_pass=True,
+        threshold=10,
+        allowed_types=["str"],
+    )
+
+    assert isinstance(
+        col_vals_regex.test_unit_res,
+        pd.DataFrame if tbl_fixture == "tbl_pd" else pl.DataFrame,
+    )
+
+    if tbl_fixture == "tbl_pd":
+        assert col_vals_regex.test_unit_res.columns.tolist() == COLUMN_LIST
+        assert col_vals_regex.get_test_results().columns.tolist() == COLUMN_LIST
+    else:
+        assert col_vals_regex.test_unit_res.columns == COLUMN_LIST
+        assert col_vals_regex.get_test_results().columns == COLUMN_LIST
+
+
+@pytest.mark.parametrize(
+    "tbl_fixture, assertion_method",
+    [
+        ("tbl_pd", "exists"),
+        ("tbl_pl", "exists"),
+    ],
+)
+def test_column_exists_has_type(request, tbl_fixture, assertion_method):
+
+    tbl = request.getfixturevalue(tbl_fixture)
+
+    col_exists_has_type = ColExistsHasType(
+        data_tbl=tbl,
+        column="x",
+        threshold=1,
+        assertion_method=assertion_method,
+    )
+
+    assert isinstance(col_exists_has_type.test_unit_res, int)
+    assert col_exists_has_type.test_unit_res == 1
