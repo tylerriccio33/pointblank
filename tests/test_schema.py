31c395642e1074cb6b36eb11f7fd6f9128016870
Richard Iannone <riannone@me.com>
2024-12-27 10:38:17 -0500

    Add several tests of Schema class

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -103,6 +103,19 @@ def test_schema_from_pl_table():
     assert str(type(schema.tbl)) == "<class 'polars.dataframe.frame.DataFrame'>"
 
 
+def test_schema_from_parquet_table(tbl_parquet):
+
+    schema = Schema(tbl=tbl_parquet)
+
+    assert schema.columns == [
+        ("x", "int64"),
+        ("y", "int64"),
+        ("z", "int64"),
+    ]
+
+    assert str(type(schema.tbl)) == "<class 'ibis.expr.types.relations.Table'>"
+
+
 def test_schema_from_duckdb_table():
     schema = Schema(tbl=load_dataset(dataset="small_table", tbl_type="duckdb"))
     assert schema.columns == [
@@ -117,6 +130,148 @@ def test_schema_from_duckdb_table():
     ]
 
     assert str(type(schema.tbl)) == "<class 'ibis.expr.types.relations.Table'>"
+
+
+def test_schema_from_sqlite_table(tbl_sqlite):
+
+    schema = Schema(tbl=tbl_sqlite)
+
+    assert schema.columns == [
+        ("x", "int64"),
+        ("y", "int64"),
+        ("z", "int64"),
+    ]
+
+    assert str(type(schema.tbl)) == "<class 'ibis.expr.types.relations.Table'>"
+
+
+def test_get_tbl_type_small_table():
+    schema_pd = Schema(tbl=load_dataset(dataset="small_table", tbl_type="pandas"))
+    schema_pl = Schema(tbl=load_dataset(dataset="small_table", tbl_type="polars"))
+    schema_duckdb = Schema(tbl=load_dataset(dataset="small_table", tbl_type="duckdb"))
+
+    assert schema_pd.get_tbl_type() == "pandas"
+    assert schema_pl.get_tbl_type() == "polars"
+    assert schema_duckdb.get_tbl_type() == "duckdb"
+
+
+def test_get_column_list_small_table():
+    schema_pd = Schema(tbl=load_dataset(dataset="small_table", tbl_type="pandas"))
+    schema_pl = Schema(tbl=load_dataset(dataset="small_table", tbl_type="polars"))
+    schema_duckdb = Schema(tbl=load_dataset(dataset="small_table", tbl_type="duckdb"))
+
+    schemas = [schema_pd, schema_pl, schema_duckdb]
+
+    for schema in schemas:
+        assert schema.get_column_list() == [
+            "date_time",
+            "date",
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+        ]
+
+
+def test_get_dtype_list_small_table_pd():
+    schema = Schema(tbl=load_dataset(dataset="small_table", tbl_type="pandas"))
+
+    assert schema.get_dtype_list() == [
+        "Datetime(time_unit='ns', time_zone=None)",
+        "Datetime(time_unit='ns', time_zone=None)",
+        "Int64",
+        "String",
+        "Float64",
+        "Float64",
+        "Boolean",
+        "String",
+    ]
+
+
+def test_get_dtype_list_small_table_pl():
+    schema = Schema(tbl=load_dataset(dataset="small_table", tbl_type="polars"))
+
+    assert schema.get_dtype_list() == [
+        "Datetime(time_unit='us', time_zone=None)",
+        "Date",
+        "Int64",
+        "String",
+        "Int64",
+        "Float64",
+        "Boolean",
+        "String",
+    ]
+
+
+def test_get_dtype_list_small_table_duckdb():
+    schema = Schema(tbl=load_dataset(dataset="small_table", tbl_type="duckdb"))
+
+    assert schema.get_dtype_list() == [
+        "timestamp(6)",
+        "date",
+        "int64",
+        "string",
+        "int64",
+        "float64",
+        "boolean",
+        "string",
+    ]
+
+
+def test_get_dtype_list_game_revenue_pd():
+    schema = Schema(tbl=load_dataset(dataset="game_revenue", tbl_type="pandas"))
+
+    assert schema.get_dtype_list() == [
+        "String",
+        "String",
+        "Datetime(time_unit='ns', time_zone='UTC')",
+        "Datetime(time_unit='ns', time_zone='UTC')",
+        "String",
+        "String",
+        "Float64",
+        "Float64",
+        "Datetime(time_unit='ns', time_zone=None)",
+        "String",
+        "String",
+    ]
+
+
+def test_get_dtype_list_game_revenue_pl():
+    schema = Schema(tbl=load_dataset(dataset="game_revenue", tbl_type="polars"))
+
+    assert schema.get_dtype_list() == [
+        "String",
+        "String",
+        "Datetime(time_unit='us', time_zone='UTC')",
+        "Datetime(time_unit='us', time_zone='UTC')",
+        "String",
+        "String",
+        "Float64",
+        "Float64",
+        "Date",
+        "String",
+        "String",
+    ]
+
+
+def test_get_dtype_list_game_revenue_duckdb():
+    schema = Schema(tbl=load_dataset(dataset="game_revenue", tbl_type="duckdb"))
+
+    assert schema.get_dtype_list() == [
+        "string",
+        "string",
+        "timestamp('UTC', 6)",
+        "timestamp('UTC', 6)",
+        "string",
+        "string",
+        "float64",
+        "float64",
+        "date",
+        "string",
+        "string",
+    ]
 
 
 @pytest.mark.parametrize("tbl_fixture", TBL_LIST)
