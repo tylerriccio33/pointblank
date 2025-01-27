0cd6ec61c86074421907bd0c341f70e20b7cb673
Richard Iannone <riannone@me.com>
2025-01-26 21:13:02 -0500

    Add snapshot test

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -6078,7 +6078,7 @@ def test_get_schema_step_report_07(tbl_schema_tests, snapshot):
 
 def test_get_schema_step_report_08(tbl_schema_tests, snapshot):
 
-    # 7. Schema has 2/3 columns matching, incorrect order; incorrect dtypes
+    # 8. Schema has 2/3 columns matching, incorrect order; incorrect dtypes
     schema = Schema(
         columns=[
             ("c", "invalid"),
@@ -6103,3 +6103,31 @@ def test_get_schema_step_report_08(tbl_schema_tests, snapshot):
 
     # Take snapshot of the report DataFrame
     snapshot.assert_match(str(report_df), "schema_step_report_08-0.txt")
+
+
+def test_get_schema_step_report_09(tbl_schema_tests, snapshot):
+
+    # 9. Schema has single column match; incorrect dtype
+    schema = Schema(
+        columns=[
+            ("c", "invalid"),
+        ]
+    )
+
+    validation = (
+        Validate(data=tbl_schema_tests)
+        .col_schema_match(
+            schema=schema,
+            complete=True,  # default
+            in_order=True,  # default
+            case_sensitive_colnames=True,  # default
+            case_sensitive_dtypes=True,  # default
+            full_match_dtypes=True,  # default
+        )
+        .interrogate()
+    )
+
+    report_df = validation.get_step_report(i=-99)
+
+    # Take snapshot of the report DataFrame
+    snapshot.assert_match(str(report_df), "schema_step_report_09-0.txt")
