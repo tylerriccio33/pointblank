0920ce62f558d43465a8b9648e75e7421e8b7dda
Richard Iannone <riannone@me.com>
2025-01-29 14:31:00 -0500

    Add some tests for get_step_report()

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -4812,6 +4812,40 @@ def test_get_step_report_no_fail(tbl_type):
         assert isinstance(validation.get_step_report(i=i), GT.GT)
 
 
+def test_get_step_report_failing_inputs():
+
+    small_table = load_dataset(dataset="small_table", tbl_type="pandas")
+
+    validation = Validate(small_table).col_vals_gt(columns="a", value=0).interrogate()
+
+    with pytest.raises(ValueError):
+        validation.get_step_report(i=0)
+
+    with pytest.raises(ValueError):
+        validation.get_step_report(i=2)
+
+
+def test_get_step_report_inactive_step():
+
+    small_table = load_dataset(dataset="small_table", tbl_type="pandas")
+
+    validation = Validate(small_table).col_vals_gt(columns="a", value=0, active=False).interrogate()
+
+    validation.get_step_report(i=1) == "This validation step is inactive."
+
+
+def test_get_step_report_non_supported_steps():
+
+    small_table = load_dataset(dataset="small_table", tbl_type="pandas")
+
+    validation = (
+        Validate(small_table).rows_distinct().rows_distinct(columns_subset=["a", "b"]).interrogate()
+    )
+
+    assert validation.get_step_report(i=1) is None
+    assert validation.get_step_report(i=2) is None
+
+
 @pytest.mark.parametrize(
     "schema",
     [
