272f9d1f7061b00d932b5bbd13eba28fe6461e5b
Richard Iannone <riannone@me.com>
2025-02-17 11:14:20 -0500

    Add import statements to assert_passing() example

--- pointblank/validate.py
+++ pointblank/validate.py
@@ -5068,6 +5068,8 @@ class Validate:
         method is used to assert that all validation steps passed perfectly.
 
         ```{python}
+        import pointblank as pb
+        import polars as pl
 
         tbl = pl.DataFrame(
             {
