5f9db0eb8972a685339b14bf16d1ed39a0852867
Tyler Riccio <tylerriccio8@gmail.com>
2025-03-19 21:57:00 -0400

    ensure debugger stops in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,14 @@
-import os
+# conftest.py
+import sys
 import pytest
 
-if os.getenv("_PYTEST_RAISE", "0") != "0":
+
+def is_debugging():
+    return "debugpy" in sys.modules
+
+
+# enable_stop_on_exceptions if the debugger is running during a test
+if is_debugging():
 
     @pytest.hookimpl(tryfirst=True)
     def pytest_exception_interact(call):
