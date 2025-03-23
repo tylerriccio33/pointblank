b5c4c17370f27229e11d28dee2499f8fd689dcfd
Tyler Riccio <tylerriccio8@gmail.com>
2025-03-23 16:05:34 -0400

    add possible depracation to test previoew

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -5246,6 +5246,7 @@ def test_preview_fails_head_tail_exceed_limit():
     preview(small_table, n_head=100, n_tail=100, limit=300)
 
 
+@pytest.mark.skip("Possibly deprecated")
 def test_preview_no_polars_duckdb_table():
     small_table = load_dataset(dataset="small_table", tbl_type="duckdb")
 
@@ -5353,6 +5354,7 @@ def test_missing_vals_tbl_no_polars():
         missing_vals_tbl(small_table)
 
 
+@pytest.mark.skip(reason="Possibly depracated")
 def test_missing_vals_tbl_using_ibis_no_pandas():
     # Mock the absence of the pandas library
     with patch.dict(sys.modules, {"pandas": None}):
@@ -5361,6 +5363,7 @@ def test_missing_vals_tbl_using_ibis_no_pandas():
         missing_vals_tbl(small_table)
 
 
+@pytest.mark.skip(reason="Possibly depracated")
 def test_missing_vals_tbl_using_ibis_no_polars():
     # Mock the absence of the polars library
     with patch.dict(sys.modules, {"polars": None}):
