c6e92224219b6be61e3f5bfd566fac90a0de7931
Tyler Riccio <tylerriccio8@gmail.com>
2025-05-22 22:26:06 -0400

    fix xfail in proc compare object

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -6,7 +6,7 @@ import polars.testing.parametric as pt
 from hypothesis import given
 
 
-@pytest.mark.xfail(NotImplementedError)
+@pytest.mark.xfail
 def test_compare_basic(dfa, dfb) -> None:
     comp = Compare(dfa, dfb)
 
