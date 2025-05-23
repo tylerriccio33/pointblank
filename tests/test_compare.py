35c7916eb470edcbe1d2608d2a9b7a034952b2ed
Tyler Riccio <tylerriccio8@gmail.com>
2025-05-22 22:20:00 -0400

    xfail test compare

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -6,11 +6,7 @@ import polars.testing.parametric as pt
 from hypothesis import given
 
 
-@given(
-    dfa=pt.dataframes(min_size=100, max_size=1_000, allow_null=False),
-    dfb=pt.dataframes(min_size=100, max_size=1_000, allow_null=False),
-)
-@pytest.mark.skip(reason="Not implemented")
+@pytest.mark.xfail(NotImplementedError)
 def test_compare_basic(dfa, dfb) -> None:
     comp = Compare(dfa, dfb)
 
