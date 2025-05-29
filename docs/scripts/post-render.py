822cdb663f72fa162a62e041844cdd16b10e4605
Richard Iannone <riannone@me.com>
2025-05-29 16:51:10 -0400

    Update post-render.py

--- docs/scripts/post-render.py
+++ docs/scripts/post-render.py
@@ -3,15 +3,18 @@ import os
 # Print the working directory
 print("Current working directory:", os.getcwd())
 
-# Get a list of all files in the current directory
-files = os.listdir("reference")
-print("Files in 'reference' directory:", files)
+# Get a list of all files in the working directory
+files = os.listdir(".")
+print("Files in working directory:", files)
 
-with open("docs/reference/Actions.html", "r") as file:
+site_files = os.listdir("_site")
+print("Files in '_site' directory:", site_files)
+
+with open("_site/reference/Actions.html", "r") as file:
     content = file.readlines()
 
 # Replace `<h1>Actions</h1>` with `<h1><code>Actions</code></h1>`
 content = [line.replace("<h1>Actions</h1>", "<h1><code>Actions</code></h1>") for line in content]
 
-with open("docs/reference/Actions.html", "w") as file:
+with open("_site/reference/Actions.html", "w") as file:
     file.writelines(content)
