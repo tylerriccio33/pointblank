d6164478663b730b62c3de429d72c3c778c4166d
Richard Iannone <riannone@me.com>
2025-05-29 17:06:12 -0400

    Update post-render.py

--- docs/scripts/post-render.py
+++ docs/scripts/post-render.py
@@ -13,8 +13,14 @@ print("Files in '_site' directory:", site_files)
 with open("_site/reference/Actions.html", "r") as file:
     content = file.readlines()
 
-# Replace `<h1>Actions</h1>` with `<h1><code>Actions</code></h1>`
-content = [line.replace("<h1>Actions</h1>", "<h1><code>Actions</code></h1>") for line in content]
+# Replace <h1> tag with a styled version
+content = [
+    line.replace(
+        '<h1 class="title">Actions</h1>',
+        "<h1 class=\"title\" style=\"font-family: SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace;\">Actions</h1>",
+    )
+    for line in content
+]
 
 with open("_site/reference/Actions.html", "w") as file:
     file.writelines(content)
