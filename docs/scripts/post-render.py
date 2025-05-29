52d1d1e23c540d266f395da6d77b57422c9f597d
Richard Iannone <riannone@me.com>
2025-05-29 16:27:18 -0400

    Modify paths

--- docs/scripts/post-render.py
+++ docs/scripts/post-render.py
@@ -1,8 +1,8 @@
-with open("_site/reference/Actions.html", "r") as file:
+with open("reference/Actions.html", "r") as file:
     content = file.readlines()
 
 # Replace `<h1>Actions</h1>` with `<h1><code>Actions</code></h1>`
 content = [line.replace("<h1>Actions</h1>", "<h1><code>Actions</code></h1>") for line in content]
 
-with open("_site/reference/Actions.html", "w") as file:
+with open("reference/Actions.html", "w") as file:
     file.writelines(content)
