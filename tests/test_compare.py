340396886d547486840e29fb1c9669c7c0944e56
Tyler Riccio <tylerriccio8@gmail.com>
2025-05-31 11:49:18 -0400

    build out compare testing

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -1,15 +1,37 @@
 from __future__ import annotations
 import pytest
+import polars as pl
 
-from pointblank.compare import Compare
-import polars.testing.parametric as pt
-from hypothesis import given
+from pointblank.compare import Compare, MetaSummary
 
 
-@pytest.mark.xfail
-def test_compare_basic(dfa, dfb) -> None:
-    comp = Compare(dfa, dfb)
+def test_compare_basic() -> None:
+    df1 = {
+        "a": [1, 2, 3],
+        "b": [4, 5, 6],
+    }
+    df2 = {
+        "a": [1, 2, 3],
+        "b": ["4", "5", "7"],
+        "c": [8, 9, 10],
+    }
+    data1 = pl.DataFrame(df1)
+    data2 = pl.DataFrame(df2)
+    comp = Compare(data1, data2)
 
     comp.compare()
 
-    raise NotImplementedError
+    ## Pull out the summary data
+    summary: MetaSummary = comp.meta_summary
+
+    assert summary.name == ["a", "b"]
+    assert summary.n_observations == (3, 3)
+    assert summary.n_variables == (2, 3)
+    assert summary.in_a_only == set()
+    assert summary.in_b_only == {"c"}
+    assert summary.in_both == {"a", "b"}
+    assert summary.conflicting_types == ["b"]
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-v"])
