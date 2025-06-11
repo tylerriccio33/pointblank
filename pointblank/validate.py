92717439443734aaa691e1551437db763db4f38e
Richard Iannone <riannone@me.com>
2025-06-11 16:28:02 -0400

    Modify description of data param in Validate

--- pointblank/validate.py
+++ pointblank/validate.py
@@ -1973,10 +1973,12 @@ class Validate:
     Parameters
     ----------
     data
-        The table to validate, which could be a DataFrame object, an Ibis table object, or a CSV
-        file path. When providing a CSV file path (as a string or `pathlib.Path` object), the file
-        will be automatically loaded using an available DataFrame library (Polars or Pandas). Read
-        the *Supported Input Table Types* section for details on the supported table types.
+        The table to validate, which could be a DataFrame object, an Ibis table object, a CSV
+        file path, or a Parquet file path. When providing a CSV or Parquet file path (as a string
+        or `pathlib.Path` object), the file will be automatically loaded using an available
+        DataFrame library (Polars or Pandas). Parquet input also supports glob patterns and
+        directories containing .parquet files. Read the *Supported Input Table Types* section for
+        details on the supported table types.
     tbl_name
         An optional name to assign to the input table object. If no value is provided, a name will
         be generated based on whatever information is available. This table name will be displayed
